1d4778aac75e35aaf84689896794d60b8dae5b62
Lê Quang Vũ <vulq2@vng.com.vn>
2020-08-30 18:39:07 +0700

    api: Remove upload file route

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,17 +2,26 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 
-import { uploadFileHandler } from './routes/uploadFile';
-
-const PORT = process.env.PORT || 4999;
-
 const app = express();
-app.use(cors());
+
+app.use(cors([
+    /http:\/\/localhost:?\d+/i,
+    "https://remtori.netlify.app",
+    "https://files-remtori.netlify.app",
+]));
+
 app.use(morgan('short'));
 app.use(express.json());
 
-app.use('/uploadFile', uploadFileHandler);
 
-app.listen(PORT, () => {
-    console.log('Server started! Listening at port: ' + PORT);
-});
+if (process.env.NODE_ENV !== 'production')
+{
+    const PORT = process.env.PORT || 4999;
+    app.listen(PORT, () => {
+        console.log('Server started! Listening at port: ' + PORT);
+    });
+}
+else
+{
+    module.exports.handler = require('serverless-http')(app);
+}
